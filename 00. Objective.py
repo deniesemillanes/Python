e3f760cb831393637476472b5fd0b01e80729c8f
Deniese Joie Millanes <96220674+deniesemillanes@users.noreply.github.com>
2024-04-16 04:48:23 +0800

    1

---
+++ 00. Objective.py
@@ -0,0 +1,7 @@
+#1 INTRO TO PYTHON
+"""
+-- Python's syntax, comments and execution
+-- Variables and Data Types
+-- Terminal Outputs and Typecasting
+-- Operations
+"""
---
+++ 01. INTRO_TO_PYTHON.py
@@ -0,0 +1,11 @@
+# Command to run programs in python
+"""
+on Windows:
+    python <filename.py>
+
+on Mac:
+    python3 <filename.py>
+"""
+
+# Python Syntax
+print("Hello")
